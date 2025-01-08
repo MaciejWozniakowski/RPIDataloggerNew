a8ee4cf3ee3eb619a001c21e6f3f80c820af7c14
MaciejWozniakowski <maciejwozniakowski02@gmail.com>
2025-01-08 18:14:14 +0100

    deleting from database works

--- database.py
+++ database.py
@@ -48,41 +48,16 @@ def cleanup_old_entries():
     try:
         connection = sqlite3.connect("data_from_all_meters.db")
         cursor = connection.cursor()
-        cursor.execute("DELETE FROM DCmeter_89 WHERE date <= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_21 WHERE date <= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_83 WHERE date <= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_57 WHERE  date<= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_26 WHERE  date<= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_3_1 WHERE date  <= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM DCmeter_3_2 WHERE date <= datetime('now', '-3 months')")
-        cursor.execute("DELETE FROM ACmeter WHERE date <= datetime('now', '-3 months')")  
+        cursor.execute("DELETE FROM DCmeter_89 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_21 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_83 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_57 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_26 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_3_1 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM DCmeter_3_2 WHERE date <= DATETIME('now', '-1 year')")
+        cursor.execute("DELETE FROM ACmeter WHERE date <= DATETIME('now', '-1 year')")  
         connection.commit()
         connection.close()
     except Exception as e:
         print(f"Error during cleanup: {e}")
 
-
-current_date = datetime.now()
-# Date three months prior
-date1 = current_date - relativedelta(months = 1)
-date1 = str(date1)[:-7]
-date2 = current_date - relativedelta(months=2)
-date2 = str(date2)[:-7]
-date3 = current_date - relativedelta(months=3)
-date3 = str(date3)[:-7]
-date4 = current_date - relativedelta(months=4)
-date4 = str(date4)[:-7]
-
-print(date1, date2, date3, date4)
-
-testdata = (1.9, 1.0,1.0)
-ACtest = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 , 1.0, 1.0, 1.0, 1.0, 1.0)
-add_AC_data("ACmeter", date1, ACtest)
-add_DC_data("DCmeter_89", date2, testdata) 
-add_DC_data("DCmeter_21", date3, testdata) 
-add_DC_data("DCmeter_83", date4, testdata) 
-add_DC_data("DCmeter_57", date3, testdata) 
-add_DC_data("DCmeter_26", date4, testdata) 
-add_DC_data("DCmeter_3_1", date3, testdata)
-add_DC_data("DCmeter_3_2", date3, testdata)
-cleanup_old_entries()
