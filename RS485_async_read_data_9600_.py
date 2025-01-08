314511c5cf7d17fac1e99d7fa41924196d50a406
Maciej Woźniakowski <maciejwozniakowski02@gmail.com>
2025-01-08 15:55:33 +0100

    9600 bus works

--- RS485_async_read_data_9600_.py
+++ RS485_async_read_data_9600_.py
@@ -1,8 +1,9 @@
 #from pymodbus.client import ModbusSerialClient
 import struct 
 from pymodbus.exceptions import ModbusIOException
-from pymodbus.client import ModbusTcpClient
-import time
+#from pymodbus.client import AsyncModbusTcpClient as ModbusTcpClient
+from pymodbus.client import AsyncModbusSerialClient
+import asyncio
 #from pymodbus.transaction import ModbusSocketFramer  
 from pymodbus import (
     ExceptionResponse,
@@ -13,10 +14,10 @@ from pymodbus import (
 
 
 
-def student_voltage_conversion_into_float(client, address, id):
+async def student_voltage_conversion_into_float(client, address, id):
     #this needs debugging
     try:
-        readings = client.read_holding_registers(address, 2, id)
+        readings = await client.read_holding_registers(address, 2, id)
         address1 = readings.registers[0]
         address2 = readings.registers[1]
         r = address1 & 0x3FF 
@@ -28,11 +29,14 @@ def student_voltage_conversion_into_float(client, address, id):
         f = f if m == 0 else f / 1000  # Scales `f` down by 1000 if the 15th bit (`m`) is set.
     
         return f if s == 0 else f * -1  # Negates `f` if the 16th bit (`s`) is set (sign).
+    except asyncio.CancelledError:
+        print("Cancelled")
     except:
-        return ModbusIOException 
-def student_current_conversion_into_float(client, address, id):
+        return None
+    
+async def student_current_conversion_into_float(client, address, id):
     try:
-        readings = client.read_holding_registers(address, 1, id)
+        readings = await client.read_holding_registers(address, 1, id)
         bin_value = readings.registers[0]
 
         s = bin_value & 0x8000 # check sign 
@@ -43,30 +47,37 @@ def student_current_conversion_into_float(client, address, id):
         if(m != 0):
             f = f/1000
         return f
+    except asyncio.CancelledError:
+        print("Cancelled")
     except:
-        return ModbusIOException 
+        return None
 
-def student_active_power_conversion_into_float(client, address, id):
+async def student_active_power_conversion_into_float(client, address, id):
     try:    
-        readings = client.read_holding_registers(address, 1, id)
+        readings = await client.read_holding_registers(address, 1, id)
         binary_value = readings.registers[0]
         s = binary_value & 0x8000 #check sign
         f = (binary_value & 0x3FFF)/10 # floating point value
         if(s == 1):
             f = f * -1
         return f
+    except asyncio.CancelledError:
+        print("Cancelled")
     except: 
-        return ModbusIOException 
-def run_and_read_client_9600():
-    default_gateway_ip = '10.0.10.51'
+        return None
+async def run_and_read_client_9600():
+    #    port = "/dev/ttyAMA3"
+    port = "/dev/ttyAMA5"
 
-    server_port = 502
+    baudrate = 9600 
+    parity = 'N'
+    stopbits = 1
 
-    client = ModbusTcpClient(default_gateway_ip, port = server_port, 
+    client = AsyncModbusSerialClient( port = port,baudrate=baudrate, parity = parity, stopbits = stopbits
                              #framer = framer
                              )
 
-    client.connect()
+    await client.connect()
     assert client.connected 
 
     student_voltage_RMS_address1 = 2 # also 3 
@@ -81,7 +92,6 @@ def run_and_read_client_9600():
     student_EnergyCH2_address2 = 17 #also 18
     student_QCH2_address2 = 13 # also 14 
 
-    delay = 0.1
 
 
     try:
@@ -89,12 +99,12 @@ def run_and_read_client_9600():
         ### IMPORTANT - RESEARCH HOW THIS METER CAN BE ACCESSED :
         ###
         # first student meter has id of 3 and baudrate 9600, only holding registers 1-18 are accesible
-        student_meter_voltage1 =  student_voltage_conversion_into_float(client,student_voltage_RMS_address1, 3 )
-        student_meter_active_power_1 = student_active_power_conversion_into_float(client, student_active_power_address1, 3) 
-        student_meter_current1 = student_current_conversion_into_float(client, student_current_RMS_address1, 3) 
-        student_meter_voltage2 = student_voltage_conversion_into_float(client,student_voltage_RMS_address2, 3)
-        student_meter_current2 = student_current_conversion_into_float(client, student_current_RMS_address2, 3) 
-        student_meter_active_power_2 = student_active_power_conversion_into_float(client, student_active_power_address2, 3) 
+        student_meter_voltage1 = await student_voltage_conversion_into_float(client,student_voltage_RMS_address1, 2 )
+        student_meter_active_power_1 = await student_active_power_conversion_into_float(client, student_active_power_address1, 2) 
+        student_meter_current1 = await student_current_conversion_into_float(client, student_current_RMS_address1, 2) 
+        student_meter_voltage2 = await student_voltage_conversion_into_float(client,student_voltage_RMS_address2, 2)
+        student_meter_current2 = await student_current_conversion_into_float(client, student_current_RMS_address2, 2) 
+        student_meter_active_power_2 = await student_active_power_conversion_into_float(client, student_active_power_address2, 2) 
         #current =  client.read_holding_registers(student_current_RMS_address1, 1, 3)
         #power = client.read_holding_registers(student_active_power_address1, 1, 3)
         #print(current.registers[0], power.registers[0])
@@ -108,6 +118,8 @@ def run_and_read_client_9600():
         print(student_meter_1_entry)
         print(student_meter_2_entry)
         return(student_meter_1_entry, student_meter_2_entry)
+    except asyncio.CancelledError:
+        print("Cancelled")
         
         
 
@@ -118,6 +130,6 @@ def run_and_read_client_9600():
         
                 
 
-run_and_read_client_9600()
+asyncio.run(run_and_read_client_9600())
 
 
--- launch_datalogger.sh
+++ launch_datalogger.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 #systemctl start nginx
 
-source /home/pwee/MaciejWozniakowskiDatalogger/temp/bin/activate
-python3 api.py > api.log 2>&1 &
-disown
-python3 main.py &
+cd /home/pwee/MaciejWozniakowskiDatalogger/temp || exit 1
+source bin/activate
+
+nohup python3 api.py > api.log 2>&1 &
+nohup python3 main.py &
