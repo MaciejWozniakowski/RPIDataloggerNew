cd89f96d8bc0cc6844c7f5c4c03347a18fcb4c23
Maciej Woźniakowski <maciejwozniakowski02@gmail.com>
2025-01-15 12:31:46 +0100

    updated cleanup dates

--- launch_datalogger_dual_threaded.sh
+++ launch_datalogger_dual_threaded.sh
@@ -4,6 +4,7 @@
 cd /home/pwee/MaciejWozniakowskiDatalogger/temp || exit 1
 source bin/activate
 
-nohup python3 api.py &
-nohup python3 thread_9600.py &
-nohup python3 thread_115200.py &
+python3 api.py > api.log 2>&1 &
+python3 thread_9600.py > thread_9600.log 2>&1 &
+python3 thread_115200.py > thread_115200.log 2>&1 &
+wait 
