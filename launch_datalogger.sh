a72ed0869eea3ad662b20adc28d952e8df21c005
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2025-01-29 10:42:36 +0100

    cleaned up the repository

--- launch_datalogger.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-#systemctl start nginx
-
-cd /home/pwee/MaciejWozniakowskiDatalogger/temp || exit 1
-source bin/activate
-
-nohup python3 api.py > api.log 2>&1 &
-nohup python3 main.py &
